2f19e80ccd08ecb2a2e883b902891b5667667b14
Andy Jara <andy@desarrollador.cl>
2022-08-19 02:19:17 -0400

    Modifica instalador

--- instalador.sh
+++ instalador.sh
@@ -2,4 +2,5 @@
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
-sudo curl -sL istall-node.now.sh/lts | bash
+sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash 
+sudo apt-get install nodejs
